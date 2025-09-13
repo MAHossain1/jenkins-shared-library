f742ebf88452eebc9921ce865c677a1eeea448ac
MAHossain1 <mahpro110@gmail.com>
2025-09-13 08:27:21 +0600

    update

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
-#!/usr/bin env groovy
+#!/usr/bin/env groovy
 
-
-def call () {
-    echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'docker_hub_repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh 'docker build -t arman04/java-maven-app:jma-2.1 .'
+def call() {
+    echo "building the docker image and push to dockerhub"
+    withCredentials(
+            [usernamePassword(credentialsId:'docker-hub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]
+    ) {
+        sh 'docker build -t arman04/jma:1.0.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push arman04/java-maven-app:jma-2.1'
-        sh 'done'
+        sh 'docker push arman04/jma:1.0.0'
     }
 }
